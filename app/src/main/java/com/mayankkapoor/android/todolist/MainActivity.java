8a1741aa5eee133f77cfd4381c7294a154d25cf2
Mayank Kapoor <mayankkapoormail@gmail.com>
2016-07-20 00:30:09 +0530

    Add alert dialog to get todo list item (#2)
    Clicking on the plus sign will now show an alert dialog with a space to
    enter the text of the todo item. Clicking on Add will log the todo
    item. In the future, we intend to store it in a database.

--- app/src/main/java/com/mayankkapoor/android/todolist/MainActivity.java
+++ app/src/main/java/com/mayankkapoor/android/todolist/MainActivity.java
@@ -1,10 +1,13 @@
 package com.mayankkapoor.android.todolist;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -25,7 +28,21 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_add_task:
-                Log.d(TAG, "Add a new task");
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do you want to do next?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                Log.d(TAG, "Task to add: " + task);
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
                 return true;
 
             default:
